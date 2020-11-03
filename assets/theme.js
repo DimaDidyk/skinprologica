a9ca8fb09c1215292449506b4213e4c9baf2293a
DimaDidyk <didykdima11@gmail.com>
2020-11-03 19:56:44 +0200

    created section: Testimonials

---
+++ assets/theme.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper('.swiper-container', {
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    navigation: {
+        nextEl: '#testimonials-slider-next',
+        prevEl: '#testimonials-slider-prev',
+    }
+});
